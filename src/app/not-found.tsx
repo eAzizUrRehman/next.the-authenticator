036f9f1a477e48016b2f76441e71e52f991e39be
Aziz Ur Rehman <eazizurrehman@gmail.com>
2025-02-26 06:49:47 +0500

    Refine 404 Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
@@ -7,9 +8,14 @@ export default function NotFound() {
 
   return (
     <div className="flex size-full items-center justify-center">
-      <div className="mx-auto flex size-fit flex-col items-center gap-y-5 rounded-xl border border-gray-500 p-10">
-        <h2 className="w-fit text-lg font-semibold">Not Found</h2>
-        <p>Could not find this page</p>
+      <div className="mx-auto flex size-fit flex-col items-center gap-y-5 rounded-xl xxs:border xxs:p-5 md:p-10">
+        <Image
+          src="/gif/panda-404-not-found.gif"
+          alt="404 not found"
+          width={400}
+          height={400}
+        />
+
         <Button onClick={() => router.back()}>Go Back</Button>
       </div>
     </div>
